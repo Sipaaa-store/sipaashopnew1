99994e85a40b1abd840084c2d0eb1551618cbee9
Sipaaa-store <ainunpoco@gmail.com>
2026-08-07 06:41:13 +0800

    Add files via upload

---
+++ _server-secrets.js
@@ -0,0 +1,9 @@
+"use strict";
+module.exports=Object.freeze({
+  sessionSecret:"4bfb8ef839e6d5c2fb7fd2febf45c2b47270f6e6f2e357a63e2c785d3e8f65b3",
+  allowedHosts:["sipaashop.my.id","www.sipaashop.my.id"],
+  pakasirProject:"sipaaa-storee",
+  apiId:"134358",
+  apiKey:"0pgetx-wzr2sr-hdxhaf-gz8e2x-suoxhg",
+  adminPasswordHash:"37027bd355e4ff8693ea5d8c0588eb0e:285dc700aab7dcf139a3a6592b9c4d0aeb74695cd85d9eb6457456c8f94fe6352f9d0fa41dbab36781d7a68a5538ac56779e0790342b5013998634e3f1e163ec"
+});
